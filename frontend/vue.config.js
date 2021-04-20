2f39f1f69842ccc2b72349343a58a884c1264db9
SelfhostedPro <info@selfhosted.pro>
2021-04-20 08:43:33 -0700

    updated pwa support;

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+
+  pwa: {
+    name: 'Shipwright'
+  }
 };
---
+++ frontend/workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'src/',
+	globPatterns: [
+		'**/*.{vue,js,svg}'
+	],
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	],
+	swDest: 'src/sw.js'
+};
